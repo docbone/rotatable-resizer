a6b69c0cc6e6b93956cdb6f3a410096cc05c58f8
Furybean <long.zhang@ele.me>
2017-06-15 14:54:17 +0800

    add publish gh-pages build script

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -8,7 +8,7 @@ var path = require('path');
 var chalk = require('chalk');
 var webpack = require('webpack');
 var config = require('../config');
-var webpackConfig = require('./webpack.build.conf');
+var webpackConfig = require('./webpack.prod.conf');
 
 var spinner = ora('building for production...');
 spinner.start();
@@ -19,17 +19,19 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     spinner.stop();
     if (err) throw err;
     process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n');
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+      }) + '\n\n');
 
     console.log(chalk.cyan('  Build complete.\n'));
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
+
+    var ghpages = require('gh-pages');
+
+    ghpages.publish('dist', function(err) {
+      console.log(chalk.cyan('  Publish complete.\n'));
+    });
   });
 });
